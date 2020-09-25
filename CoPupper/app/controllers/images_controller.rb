640e10dd26b53f85cae69290922981acb31f5072
Andrew Moroco <andrew.moroco@gmail.com>
2020-09-25 09:04:05 -0700

    updating image controller and hiding cloudinary keys in .env

--- CoPupper/app/controllers/images_controller.rb
+++ CoPupper/app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController < ApplicationController
     def create 
-        # prevAva = Image.where(is_avatar: true)
-        # prevAva.not_avatar
+        prevAva = Image.find_by(is_avatar: true, copupper_id: params[:copupper_id])
+        prevAva.not_avatar
         image = Cloudinary::Uploader.upload(params[:image])
         img = Image.create(url: image["url"], is_avatar: true, copupper_id: params[:copupper_id])
         render json: img
