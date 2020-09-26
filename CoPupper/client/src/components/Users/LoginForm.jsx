6b4060edb374997491f1975831ac2cd1be3910f6
Andrew Moroco <andrew.moroco@gmail.com>
2020-09-25 19:44:39 -0700

    updating styling for login and register pages

--- CoPupper/client/src/components/Users/LoginForm.jsx
+++ CoPupper/client/src/components/Users/LoginForm.jsx
@@ -9,11 +9,13 @@ const LoginForm = ({ handleLoginSubmit }) => {
     return (
         <div className="not-logged-in">
             <aside className="left-aside">
-                <h1>Welcome to the CoPuppers!</h1>
-                <form onSubmit={(evt) => handleLoginSubmit(evt, { username, password })}>
-                    <input type="text" name="username" placeholder="Username" value={username} onChange={(evt) => setUsername(evt.target.value)} />
-                    <input type="password" name="password" placeholder="Password" value={password} onChange={(evt) => setPassword(evt.target.value)} />
-                    <input type="submit" value="Log In" />
+                <img src="/copuppers-logo-light-text.png" />
+                <form onSubmit={(evt) => handleLoginSubmit(evt, { username, password })} className="login-form">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Username" value={username} onChange={(evt) => setUsername(evt.target.value)} />
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={(evt) => setPassword(evt.target.value)} />
+                    <input type="submit" value="Log In" className="login-register-button"/>
                     {Auth.isUserAuthenticated() && <Redirect oush to='/profile' />}
                 </form>
                 <p>Note a member yet? Register <Link to="/register">here</Link></p>
--- CoPupper/client/src/components/Users/RegisterForm.jsx
+++ CoPupper/client/src/components/Users/RegisterForm.jsx
@@ -12,14 +12,19 @@ const RegisterForm = ({ handleRegisterSubmit }) => {
     return (
         <div className="not-logged-in">
             <aside className="left-aside">
-                <h1>Welcome to the CoPuppers!</h1>
-                <form onSubmit={(evt) => handleRegisterSubmit(evt, { username, password, email, first_name, last_name })}>
-                    <input type="text" name="username" placeholder="Username" value={username} onChange={(evt) => setUsername(evt.target.value)} />
-                    <input type="password" name="password" placeholder="Password" value={password} onChange={(evt) => setPassword(evt.target.value)} />
-                    <input type="test" name="email" placeholder="Email" value={email} onChange={(evt) => setEmail(evt.target.value)} />
-                    <input type="test" name="first_name" placeholder="First Name" value={first_name} onChange={(evt) => setFirst_Name(evt.target.value)} />
-                    <input type="test" name="last_name" placeholder="Last Name" value={last_name} onChange={(evt) => setLast_Name(evt.target.value)} />
-                    <input type="submit" value="Register" />
+                <img src="/copuppers-logo-light-text.png" />
+                <form onSubmit={(evt) => handleRegisterSubmit(evt, { username, password, email, first_name, last_name })} className="register-form">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Username" value={username} onChange={(evt) => setUsername(evt.target.value)} />
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Password" value={password} onChange={(evt) => setPassword(evt.target.value)} />
+                    <label for="email">Email</label>
+                    <input type="test" id="email" name="email" placeholder="Email" value={email} onChange={(evt) => setEmail(evt.target.value)} />
+                    <label for="first_name">First Name</label>
+                    <input type="test" id="first_name" name="first_name" placeholder="First Name" value={first_name} onChange={(evt) => setFirst_Name(evt.target.value)} />
+                    <label for="last_name">Last Name</label>
+                    <input type="test" id="last_name" name="last_name" placeholder="Last Name" value={last_name} onChange={(evt) => setLast_Name(evt.target.value)} />
+                    <input type="submit" value="Register" className="login-register-button" />
                     {Auth.isUserAuthenticated() && <Redirect oush to='/profile' />}
                 </form>
             </aside>
