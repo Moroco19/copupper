b13efe986c1b44b547bbac2ce163bd3bf5d9b585
Andrew Moroco <andrew.moroco@gmail.com>
2020-09-27 12:25:06 -0700

    coppuper profile page styling

--- CoPupper/client/src/components/Copuppers/CopupperProfile.jsx
+++ CoPupper/client/src/components/Copuppers/CopupperProfile.jsx
@@ -34,14 +34,31 @@ const CoPupperProfile = ({ match }) => {
     }
 
     return (
-        <div>
-            <h1>Hi from CoPupper {copupper.name}'s Profile</h1>
-            <img src={profAva ? profAva.url : "/no_avatar.png"} className="copupper-avatar" />
-            <form onSubmit={submitAvatar}>
-                <input type="file" name="image" onChange={(evt) => setAvatar(evt.target.files[0])} />
-                <input type="submit" value="Upload Avatar" />
-            </form>
-        </div>
+        <main className="copupper-profile-container">
+            <aside className="copupper-profile-aside">
+                <div className="copupper-profile-aside-container">
+                    <img src={profAva ? profAva.url : "/no_avatar.png"} className="copupper-avatar profile-page-ava" />
+                    <form onSubmit={submitAvatar}>
+                        <input type="file" name="image" onChange={(evt) => setAvatar(evt.target.files[0])} />
+                        <input type="submit" value="Upload Avatar" />
+                    </form>
+                    <h3>Hi! I am {copupper.name}!</h3>
+                    <p>My trainer tells me I am a {copupper.breed}, isn't that neat?</p>
+                </div>
+            </aside>
+            <section className="copupper-profile-section">
+                <summary className="copupper-profile-summary">
+                    <h3>Summary:</h3>
+                    <p>You can find me in the {copupper.office_id} office!</p>
+                    <p>I usually hang out with my friends in the {copupper.department_id} department!</p>
+                </summary>
+                <article className="copupper-profile-article">
+                    <p>I am {copupper.age} years old!  My trainer says that's {copupper.age * 7} in pupper years.</p>
+                    <p>Additional CoPupper detail features (ex. friendly with others, walk times, preferred diet, ok for treats...etc)</p>
+                </article>
+            </section>
+            
+        </main>
     )
 }
 
